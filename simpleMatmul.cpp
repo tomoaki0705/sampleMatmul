4686f2442cf26c40fe891b1354f3395530b98045
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2018-03-27 17:16:45 +0900

    clean up

--- simpleMatmul.cpp
+++ simpleMatmul.cpp
@@ -5,6 +5,21 @@ cv::String keys =
     "{size s|1024|size of matrix}"
     "{help h|false|help message}"
     "{@input||\"gpu\" or other}";
+const unsigned int initialSeed = 0x7777777;
+
+int64 processMatrix(int size)
+{
+    using namespace cv;
+    Mat mat1(size, size, CV_32FC1);
+    Mat mat2(size, size, CV_32FC1);
+    RNG rng(initialSeed);
+    rng.fill(mat1, RNG::NORMAL, CV_32FC1, 10.0, 5.0);
+    rng.fill(mat2, RNG::NORMAL, CV_32FC1, 10.0, 5.0);
+    int64 start = getTickCount();
+    Mat result = mat1 * mat2;
+    int64 stop = getTickCount();
+    return stop - start;
+}
 
 int main(int argc, char**argv)
 {
@@ -15,15 +30,8 @@ int main(int argc, char**argv)
         return 0;
     }
     int size = parser.get<int>("size");
-    cv::Mat mat1(size, size, CV_32FC1);
-    cv::Mat mat2(size, size, CV_32FC1);
-    cv::RNG rng(0x7777777);
-    rng.fill(mat1, cv::RNG::NORMAL, CV_32FC1, 10.0, 5.0);
-    rng.fill(mat2, cv::RNG::NORMAL, CV_32FC1, 10.0, 5.0);
-    int64 a = cv::getTickCount();
-    cv::Mat result = mat1 * mat2;
-    int64 b = cv::getTickCount();
-    std::cout << (b - a) / cv::getTickFrequency() << std::endl;
+    int64 duration = processMatrix(size);
+    std::cout << size << '\t' << duration/cv::getTickFrequency() << 's' << std::endl;
 
     return 0;
 }
