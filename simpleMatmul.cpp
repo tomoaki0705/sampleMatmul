aef87b8a9fb1c0189dcaacd3db3f18c9522eaddc
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2018-03-27 17:49:25 +0900

    add GPU version

--- simpleMatmul.cpp
+++ simpleMatmul.cpp
@@ -10,18 +10,40 @@ cv::String keys =
     "{iteration n|10|number of iteration}";
 const unsigned int initialSeed = 0x7777777;
 
-int64 processMatrix(int size)
+void processMatrix(int size, bool useGpu, int64& duration0, int64& duration1)
 {
     using namespace cv;
-    Mat mat1(size, size, CV_32FC1);
-    Mat mat2(size, size, CV_32FC1);
-    RNG rng(initialSeed);
-    rng.fill(mat1, RNG::NORMAL, CV_32FC1, 10.0, 5.0);
-    rng.fill(mat2, RNG::NORMAL, CV_32FC1, 10.0, 5.0);
-    int64 start = getTickCount();
-    Mat result = mat1 * mat2;
-    int64 stop = getTickCount();
-    return stop - start;
+    if(useGpu)
+    {
+        Mat mat1(size, size, CV_32FC1);
+        Mat mat2(size, size, CV_32FC1);
+        UMat umat1, umat2;
+        RNG rng(initialSeed);
+        rng.fill(mat1, RNG::NORMAL, CV_32FC1, 10.0, 5.0);
+        rng.fill(mat2, RNG::NORMAL, CV_32FC1, 10.0, 5.0);
+        int64 start0 = getTickCount();
+        mat1.copyTo(umat1);
+        mat2.copyTo(umat2);
+        int64 start = getTickCount();
+        UMat result;
+        gemm(umat1, umat2, 1, noArray(), 0, result);
+        int64 stop = getTickCount();
+        duration0 = start - start0;
+        duration1 = stop - start;
+    }
+    else
+    {
+        Mat mat1(size, size, CV_32FC1);
+        Mat mat2(size, size, CV_32FC1);
+        RNG rng(initialSeed);
+        rng.fill(mat1, RNG::NORMAL, CV_32FC1, 10.0, 5.0);
+        rng.fill(mat2, RNG::NORMAL, CV_32FC1, 10.0, 5.0);
+        int64 start = getTickCount();
+        Mat result = mat1 * mat2;
+        int64 stop = getTickCount();
+        duration0 = 0;
+        duration1 = stop - start;
+    }
 }
 
 void computeMeasurement(std::vector<int64>& result, double& min, double& max, double& average, double& median, double& initial)
@@ -35,13 +57,14 @@ void computeMeasurement(std::vector<int64>& result, double& min, double& max, do
     median = (double)result[result.size()/2]/cv::getTickFrequency();
 }
 
-void doMeasurement(int size, int iteration)
+void doMeasurement(int size, int iteration, bool useGpu)
 {
     std::vector<int64> result;
     for(auto i = 0;i < iteration;i++)
     {
-        int64 duration = processMatrix(size);
-        result.push_back(duration);
+        int64 duration0, duration1;
+        processMatrix(size, useGpu, duration0, duration1);
+        result.push_back(duration1);
     }
     double min, max, average, median, initial;
     computeMeasurement(result, min, max, average, median, initial);
@@ -59,7 +82,7 @@ int main(int argc, char**argv)
     int size = 4;
     for( ;size <= parser.get<int>("size");size *= 2)
     {
-        doMeasurement(size, parser.get<int>("iteration"));
+        doMeasurement(size, parser.get<int>("iteration"), parser.get<cv::String>("@input").compare("gpu") == 0);
     }
 
     return 0;
